94618188b77d1d12de10cab84bf599e4d12e6153
kyledinh <kyledinh@gmail.com>
2013-07-08 11:09:58 -0700

    initial checkin.

---
+++ go/websocketchat/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"code.google.com/p/go.net/websocket"
+	"flag"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+var homeTempl = template.Must(template.ParseFiles("home.html"))
+
+func homeHandler(c http.ResponseWriter, req *http.Request) {
+	homeTempl.Execute(c, req.Host)
+}
+
+func main() {
+	flag.Parse()
+	go h.run()
+	http.HandleFunc("/", homeHandler)
+	http.Handle("/ws", websocket.Handler(wsHandler))
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
