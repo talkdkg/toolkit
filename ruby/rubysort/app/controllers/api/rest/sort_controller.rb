2728d87ee41b5ccb83d6bf3cc01c71705168fe0e
Kyle Dinh <kyledinh@gmail.com>
2013-09-29 00:20:43 +0000

    added NATIVE sort logic

--- ruby/rubysort/app/controllers/api/rest/sort_controller.rb
+++ ruby/rubysort/app/controllers/api/rest/sort_controller.rb
@@ -1,8 +1,9 @@
 module Api 
    module Rest 
+
       class SortController < ApplicationController
 
-      	skip_before_filter :verify_authenticity_token
+      	 skip_before_filter :verify_authenticity_token
 
          # GET method 
          def index
@@ -12,19 +13,27 @@ module Api
 
          # POST method 
          def create
-            _start_sw = Time.now
+
             @task = Task.new
 
             _raw_post = request.raw_post
             _info = JSON.parse(_raw_post)
+            _start_sw = Time.now
 
-            @task.action = "NATIVE" #_info["action"]
+            @task.action = _info["action"]
             @task.data = _info["data"]
-            @task.result = @task.data.sort
-            @task.message = "#{@task.action} #{(Time.now - _start_sw).to_s} secs "
-            @task.message += ": #{@task.result.length.to_s} elements"   
 
-            #@task.message = "This is a test."
+            case _info["action"]
+            when "NATIVE"
+               @task.result = @task.data.sort
+               @task.message = "#{@task.action} #{(Time.now - _start_sw).to_s} secs "
+               @task.message += ": #{@task.result.length.to_s} elements"   
+               @task.status = "success"
+            else 
+               @task.message = "Unsupported action type, try NATIVE"
+               @task.status = "error"
+            end  
+
             render :json => @task
          end
 
--- ruby/rubysort/app/models/delta.rb
+++
@@ -1,5 +0,0 @@
-class Util 
-   def name 
-      puts "This is Delta"
-   end
-end
