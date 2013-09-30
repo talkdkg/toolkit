d76ca13d0d298c99a3a647f917ce762fba556b36
Kyle Dinh <kyledinh@gmail.com>
2013-09-30 00:38:15 +0000

    json payload parsing/error check

--- ruby/rubysort/app/controllers/api/rest/sort_controller.rb
+++ ruby/rubysort/app/controllers/api/rest/sort_controller.rb
@@ -13,29 +13,41 @@ module Api
 
          # POST method 
          def create
-
             @task = Task.new
+             _start_sw = Time.now
+            logger.debug "Request ==== #{ request.params } "            
+            _payload = request.raw_post
+            _param_sort = request.params[:sort]
+            logger.debug "param_sort ... #{ _param_sort }> "
 
-            _raw_post = request.raw_post
-            _info = JSON.parse(_raw_post)
-            _start_sw = Time.now
-
-            @task.action = _info["action"]
-            @task.data = _info["data"]
-
-            case _info["action"]
-            when "NATIVE"
-               @task.result = @task.data.sort
-               @task.message = "#{@task.action} #{(Time.now - _start_sw).to_s} secs "
-               @task.message += ": #{@task.result.length.to_s} elements"   
-               @task.status = "success"
-            else 
-               @task.message = "Unsupported action type, try NATIVE"
+            if (_payload.nil? && _param_sort.nil?)  
+               @task.message = "No raw_post data found in Request."
                @task.status = "error"
-            end  
+            else  
+               #  use params "sort" if the payload was not in the body 
+               #  work around to use curl --data that places json in the params
+               if (_payload.nil? && !_param_sort.nil?) 
+                  _info = _param_sort 
+               else
+                  _info = JSON.parse(_payload)
+               end 
 
+               @task.data = _info["data"]
+               @task.action = _info["action"]
+
+               case @task.action
+               when "NATIVE"
+                  @task.result = @task.data.sort
+                  @task.message = "#{@task.action} #{(Time.now - _start_sw).to_s} secs "
+                  @task.message += ": #{@task.result.length.to_s} elements"   
+                  @task.status = "success"
+               else 
+                  @task.message = "Unsupported action type, try NATIVE"
+                  @task.status = "error"
+               end  
+            end 
             render :json => @task
-         end
+         end #  create
 
       end
    end
