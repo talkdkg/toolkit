eee0f1fdfed244df09cdc387bc887a07457621c3
kyledinh <kyledinh@gmail.com>
2013-12-05 18:38:21 -0800

    passes treeify

--- problems/subsegment/subsegment.py
+++ problems/subsegment/subsegment.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import pprint
+import math
 
 def trimWords(words):
    return words.replace(',', '').replace('.','').lower()
@@ -21,6 +22,16 @@ def readFileToArray(path):
    f.close()
    return arr
 
+def treeify(arr, num, top, pos):
+   quotient = num / top
+   remainder = num % top
+   arr[pos] = remainder
+   if (quotient < top):
+      arr[pos+1] = quotient
+   if (quotient >= top):
+      treeify(arr, quotient, top, pos+1)
+   return arr
+
 
 ## Written by: Kyle Dinh @ https://github.com/kyledinh/toolkit/tree/master/problems/subsegment 
 ## keywordArr holds the wanted elements for the sub-segment
@@ -35,28 +46,6 @@ breath = maxCountInArrays(keywordArr, segmentArr)
 # Build matrix with the default -1 value
 matrix = [[-1 for i in range(breath)] for i in range(depth)]
 
-pprint.pprint(matrix)
-pprint.pprint(keywordArr)
-pprint.pprint(segmentArr)
-
-
-for x in range(0, depth):
-   print("> row of matrix for  "  + " ".join(str(i) for i in matrix[x]))
-
-# Update matrix with index of keyword finds
-
-for x in range(0, depth):
-   y = 0
-   for i in range(0, len(segmentArr)):
-      if segmentArr[i] == keywordArr[x]:
-         print("Found x: " + str(x) + " y:" + str(y) + " @ " + str(i) + "  " + str(segmentArr[i]) + " for " + str(keywordArr[x]))
-         matrix[x][y] = i
-         y += 1  
-
-for x in range(0, depth):
-   print(" row of matrix for  " + " ".join(str(i) for i in matrix[x]))
-
-pprint.pprint(matrix)
 print("matrix 0,0", matrix[0][0])
 print("matrix 3,2", matrix[3][2])
 
@@ -65,4 +54,50 @@ print("breath: ", breath)
 print(len(lines))
 print("src 1: ", segmentArr[0])
 
+pprint.pprint(matrix)
+pprint.pprint(keywordArr)
+pprint.pprint(segmentArr)
+
+# Update matrix with index of keyword finds
+
+for x in range(0, depth):
+   y = 0
+   for i in range(0, len(segmentArr)):
+      if segmentArr[i] == keywordArr[x]:
+         print("Found x: " + str(x) + " y: " + str(y) + " @ " + str(i) + "  " + str(segmentArr[i]) + " for " + str(keywordArr[x]))
+         matrix[x][y] = i
+         y += 1  
+
+for x in range(0, depth):
+   print(" row of matrix for  " + " ".join(str(i) for i in matrix[x]))
+
+pprint.pprint(matrix)
+
+permutation = [0]*depth
+pprint.pprint(permutation)
+
+results = []
+num_possible = int(math.pow(breath, depth))
+
+print("total possible: " + str(num_possible))
+
+for x in range(0, num_possible):
+   #arr = [-1 for x in range(depth)]
+   n = depth+1
+   arr = [-1]*n
+   combo = treeify(permutation, x, breath, 0)
+
+   for i in range(0, depth):
+      c = int(combo[i])
+      #print(matrix[i][c])
+      arr[i] = matrix[i][c]
+
+   distance = max(arr) - min(arr)
+   arr[depth] = distance
+
+   if (-1 not in arr):
+      results.append(arr)
+
+
+pprint.pprint(results)
 
