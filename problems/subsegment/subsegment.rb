fbc590ffbb14f79b37c4d5f91bb59b7eb0d640a0
kyledinh <kyledinh@gmail.com>
2013-12-10 10:36:25 -0800

    fixed to find shortest in results, cleanup

--- problems/subsegment/subsegment.rb
+++ problems/subsegment/subsegment.rb
@@ -1,4 +1,3 @@
-
 def maxCountInArrays(keywordArr, segmentArr)
    num = 0
    keywordArr.each do |keyword|
@@ -31,7 +30,8 @@ end
 
 def shortestSubsegment(arr)
    shortest = []
-   pos = arr[0].length -1
+   firstArr = arr[0]
+   pos = firstArr.length()-1
    num = 9999 # get max int size for ruby 
    arr.each do |combo|
       if num > combo[pos]
@@ -48,6 +48,7 @@ end
 ## ruby 1.9.3p362 (2012-12-25 revision 38607) [x86_64-darwin12.4.0]
 ############################################## 
 
+#lines = IO.readlines("alt.txt") 
 lines = IO.readlines("input.txt") 
 depth = lines[0].to_i
 keywordArr = lines.slice(1, depth).map {|x| x.chomp }
@@ -63,7 +64,7 @@ puts "breath: " + breath.to_s
 puts "possible permutations: " + num_possible.to_s
 
 # Build matrix, then update with index of keyword finds
-matrix = Array.new(4) { Array.new(3) {-1}} #[[-1,-1,-1] x4 ]
+matrix = Array.new(depth) { Array.new(breath) {-1}} #[[-1,-1,-1] x4 ]
 
 for x in 0..depth-1
    y = 0
@@ -101,18 +102,13 @@ for x in 0..num_possible -1
    if !arr.include? -1
       results.push(arr)
    end
-
 end
 
+# Final selection for results and display
 puts "Number of possible combos: " + results.length.to_s
-
 if results.length == 0
    puts "NO SUBSEGMENT FOUND"
 else
-   #puts shortestSubsegment(results)*" "
-   puts results[0]*" "
+   puts shortestSubsegment(results)*" "
 end
-
-
-
 
