da30567259a65680d96d516a130e5239b348e7bd
kyledinh <kyledinh@gmail.com>
2014-02-27 06:12:55 -0800

    added hulp.dev()
    Feature to use console.log() if DEBUG is set

--- javascript/hulp/hulp.js
+++ javascript/hulp/hulp.js
@@ -2,21 +2,30 @@
  * Hulp - Helper Util Library in Practice 
  * https://github.com/kyledinh/toolkit/tree/master/javascript/hulp
  */
-/*jslint devel: true, white: true */
 
-(function () {
+ /*jslint devel: true, white: true */
+
+ (function () {
     'use strict';
 
-    if (typeof this.hulp == 'undefined') {
+    if (typeof this.hulp == "undefined") {
 
         this.hulp = {};
+
+        this.hulp.dev = function(str) {
+            if (typeof DEBUG !== "undefined") {
+                if (DEBUG !== false) {
+                    console.log(str);              
+                    return true;
+                }
+            }
+            return false;
+        };
 
         this.hulp.getProperties = function(obj) {
             var key, properties = [];
             for (key in obj) {
-                if (typeof obj[key] != 'function') {
-                    properties.push(key);
-                }
+                if (typeof obj[key] != "function") { properties.push(key); }
             }
             return properties;
         };
@@ -24,35 +33,36 @@
         this.hulp.getMethods = function(obj) {
             var key, methods = [];
             for (key in obj) {
-                if (typeof obj[key] == 'function') {
-                    methods.push(key);
-                }
+                if (typeof obj[key] == "function") { methods.push(key); }
             }
             return methods;
         };
 
-	/* TODO write test */
-	this.hulp.getType = function(obj) {
-            var con = obj.constructor;  // returns a Function 
-	    return con.toString().substring(str.indexOf(' ')+1, str.indexOf('('));
-	}
+        this.hulp.getType = function(obj) {
+            if (typeof obj === "undefined") { return "undefined"; }
+            if (obj === null) { return "null"; }            
+            var con = obj.constructor.toString();  // returns a Function -> string
+            return con.substring(con.indexOf(' ')+1, con.indexOf('('));
+        };
 
-	/* TODO write test, this is an alt version of getType() */
-	this.hulp.getClass = function(obj) {
-  	    if (typeof obj === "undefined") { return "undefined"; }
+        this.hulp.getClass = function(obj) {
+            if (typeof obj === "undefined") { return "undefined"; }
             if (obj === null) { return "null"; }
             return Object.prototype.toString.call(obj).match(/^\[object\s(.*)\]$/)[1];
-	}
+        };
 
         this.hulp.toString = function() {
             console.log("Hulp - Helper Util -- uses:");
             console.log("hulp.getProperties(obj)");
-            console.log("hulp.getMethods(obj)");   
+            console.log("hulp.getMethods(obj)");
+            console.log("hulp.getClass(obj)");
+            console.log("hulp.getType(obj)");   
         };
 
         this.hulp.help = this.hulp.toString;
 
         console.log("`hulp` helper has been initialized to the Global Scope.");
+
     } else {
         console.log("`hulp` is already in use in the Global Scope, `hulp` object not redefined.");
     }
--- javascript/hulp/test/unit_hulp.js
+++ javascript/hulp/test/unit_hulp.js
@@ -1,51 +1,83 @@
 'use strict';
+var DEBUG;
 
 /* jasmine specs for controllers go here */
 
 describe('MASTER HULP UNIT TEST: ', function() {
 
-   var testObj = { a: "apple", b: "bat", c: "cat", 
-   d: function() { return true;} 
-};
+   var testObj = { 
+      a: "apple", b: "bat", c: "cat", 
+      d: function() { return true;} 
+   };
 
-describe('getClass from Object', function(){
-   it('gets a class', function() {
-      console.log(hulp.getClass(""));
-      expect(hulp.getClass("")).toBe("String");
-      expect(hulp.getClass(true)).toBe("Boolean");
-      expect(hulp.getClass(0)).toBe("Number");
-      expect(hulp.getClass([])).toBe("Array");
-      expect(hulp.getClass({})).toBe("Object");
-      expect(hulp.getClass(null)).toBe("null");
+   describe('getType from Object', function() {
+      it('gets a class', function() {
+         console.log(hulp.getType(""));
+         expect(hulp.getType("")).toBe("String");
+         expect(hulp.getType(true)).toBe("Boolean");
+         expect(hulp.getType(0)).toBe("Number");
+         expect(hulp.getType([])).toBe("Array");
+         expect(hulp.getType({})).toBe("Object");
+         expect(hulp.getType(null)).toBe("null");
 
-      var a = "This is a string";
-      expect(hulp.getClass(a)).toBe("String");
-      a = 42;
-      expect(hulp.getClass(a)).toBe("Number");
-
+         var a = "This is a string";
+         expect(hulp.getType(a)).toBe("String");
+         a = 42;
+         expect(hulp.getType(a)).toBe("Number");
+         console.log("PASSED: hulp.getType");
+      });
    });
-});
 
-describe('getProperties from Object', function(){
-   it('do something', function() {
-      var result = hulp.getProperties(testObj);
-      console.log("============= " + result);
-      var z = ['a', 'b', 'c'];
-      var i, n;
-      expect(result.length).toBe(3);
-      expect(result[0]).toBe('a');        
-   });
-});
+   describe('getClass from Object', function() {
+      it('gets a class', function() {
+         expect(hulp.getClass("")).toBe("String");
+         expect(hulp.getClass(true)).toBe("Boolean");
+         expect(hulp.getClass(0)).toBe("Number");
+         expect(hulp.getClass([])).toBe("Array");
+         expect(hulp.getClass({})).toBe("Object");
+         expect(hulp.getClass(null)).toBe("null");
 
-describe('getMethods from Object', function(){
-   it('do something', function() {
-      var result = hulp.getMethods(testObj);
-      console.log("============= " + result);
-      var z = ['d'];
-      var i, n;
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe('d');        
+         var a = "This is a string";
+         expect(hulp.getClass(a)).toBe("String");
+         a = 42;
+         expect(hulp.getClass(a)).toBe("Number");
+         console.log("PASSED: hulp.getClass");
+      });
    });
-});
+
+   describe('getProperties from Object', function() {
+      it('do something', function() {
+         var result = hulp.getProperties(testObj);
+         console.log("============= " + result);
+         var z = ['a', 'b', 'c'];
+         var i, n;
+         expect(result.length).toBe(3);
+         expect(result[0]).toBe('a');        
+         console.log("PASSED: hulp.getProperties");         
+      });
+   });
+
+   describe('getMethods from Object', function() {
+      it('do something', function() {
+         var result = hulp.getMethods(testObj);
+         console.log("============= " + result);
+         var z = ['d'];
+         var i, n;
+         expect(result.length).toBe(1);
+         expect(result[0]).toBe('d');
+         console.log("PASSED: hulp.getMethods");        
+      });
+   });
+
+   describe('test the dev log feature', function() {
+      it('do something', function() {
+         expect(hulp.dev("TEST")).toBe(false);
+         DEBUG = true;  // declare in Global Scope
+         expect(hulp.dev("Testing hulp.dev, works.")).toBe(true);      
+         DEBUG = false;
+         expect(hulp.dev("TEST")).toBe(false);      
+         console.log("PASSED: hulp.dev");                      
+      });
+   });
 
 });
